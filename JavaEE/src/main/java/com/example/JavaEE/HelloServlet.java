4470cd6180d07c6ba697e9dca2fdc3ef43f80adc
YasasDaraka <darakayasas@gmail.com>
2023-12-04 18:44:07 +0530

    JavaEE

--- JavaEE/src/main/java/com/example/JavaEE/HelloServlet.java
+++ JavaEE/src/main/java/com/example/JavaEE/HelloServlet.java
@@ -3,12 +3,43 @@ package com.example.JavaEE;
 import java.io.*;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.ServletException;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 
 @WebServlet(urlPatterns = "/customer")
 public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("application/json");
+        Connection con = null;
+        try {
+            con = DBConnection.getInstance().getConnection();
+            String sql = "SELECT * FROM customer";
+            String allJson= "";
+            ResultSet resultSet = con.prepareStatement(sql).executeQuery();
+            while (resultSet.next()) {
+             String id     =   resultSet.getString(1);
+             String name   =   resultSet.getString(2);
+             String address=  resultSet.getString(3);
+             double salary =   resultSet.getDouble(4);
+
+             String cusDetails = "{\"id\": \""+id+"\" ,\"name\": \""+name+"\" , \"address\": \""+address+"\" ,\"salary\": \""+salary+"\" },";
+             allJson = allJson+cusDetails;
+            }
+            String finalJson = "["+allJson.substring(0,allJson.length()-1)+"]";
+            resp.getWriter().write(finalJson);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+    @Override
     public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         String id=  req.getParameter("cusID");
         String name=  req.getParameter("cusName");
@@ -29,12 +60,16 @@ public class HelloServlet extends HttpServlet {
 
             if (isSaved){
                 System.out.println("Customer added");
+                resp.getWriter().write("true");
             }else{
                 System.out.println("Customer not added");
+                resp.getWriter().write("false");
             }
         } catch (SQLException throwables) {
+            resp.getWriter().write("false");
             throwables.printStackTrace();
         } catch (ClassNotFoundException e) {
+            resp.getWriter().write("false");
             e.printStackTrace();
         }
 
--- JavaEE/target/JavaEE-1.0.0/assets/schema/schema.sql
+++ JavaEE/target/JavaEE-1.0.0/assets/schema/schema.sql
@@ -5,5 +5,6 @@ CREATE TABLE customer(
                          cusId VARCHAR(10) NOT NULL,
                          cusName VARCHAR(30),
                          cusAddress VARCHAR(30),
-                         cusSalary DECIMAL(10)
+                         cusSalary DECIMAL(10),
+                         CONSTRAINT PRIMARY KEY (cusId)
 );
